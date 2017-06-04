70ebf7c0bec0d4d2c2f7151cbc8b3c33cdea3094
Rex Hsu <rex.swijo@gmail.com>
2017-06-04 16:37:56 +0800

    add script file

---
+++ ArrayUtility.js
@@ -0,0 +1,38 @@
+// ArrayUtility.js
+Array.prototype.removeByIndex = function(INDEX) {
+
+	var length = this.length;
+
+	if (length) {
+		var lastIndex = length - 1;
+
+		if (INDEX < lastIndex) {
+			for (var i = INDEX; i < lastIndex; i++) {
+				this[i] = this[i + 1];
+			}
+
+			this.pop();
+		} else if (INDEX === lastIndex) {
+			this.pop();
+		}
+	}
+};
+
+Array.prototype.remove = function(TARGET) {
+
+	var index = this.indexOf(TARGET);
+
+	if (index !== -1) {
+		var lastIndex = this.length - 1;
+
+		if (index < lastIndex) {
+			for (var i = index; i < lastIndex; i++) {
+				this[i] = this[i + 1];
+			}
+
+			this.pop();
+		} else if (index === lastIndex) {
+			this.pop();
+		}
+	}
+};
